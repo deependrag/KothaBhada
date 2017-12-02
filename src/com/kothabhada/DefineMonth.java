a6e0c30eb86ef91962de62af0a89e72491937a5b
ultraDCR <rparajuli4@gmail.com>
2017-12-02 15:47:56 +0545

    month conversion updated

--- src/com/kothabhada/DefineMonth.java
+++ src/com/kothabhada/DefineMonth.java
@@ -16,40 +16,40 @@ public class DefineMonth {
     static int t;
     public static int getMonth(String m){
         if(m.equals("JAN"))
-            return 0;
-        
-        else if(m.equals("FEB"))
             return 1;
         
-        else if(m.equals("MAR"))
+        else if(m.equals("FEB"))
             return 2;
         
-        else if(m.equals("APR"))
+        else if(m.equals("MAR"))
             return 3;
         
-        else if(m.equals("MAY"))
+        else if(m.equals("APR"))
             return 4;
         
-        else if(m.equals("JUNE"))
+        else if(m.equals("MAY"))
             return 5;
         
-        else if(m.equals("JULY"))
+        else if(m.equals("JUN"))
             return 6;
         
-        else if(m.equals("AUG"))
+        else if(m.equals("JUL"))
             return 7;
         
-        else if(m.equals("SET"))
+        else if(m.equals("AUG"))
             return 8;
         
-        else if(m.equals("OCT"))
+        else if(m.equals("SET"))
             return 9;
         
-        else if(m.equals("NOV"))
+        else if(m.equals("OCT"))
             return 10;
         
-        else if(m.equals("DEC"))
+        else if(m.equals("NOV"))
             return 11;
+        
+        else if(m.equals("DEC"))
+            return 12;
         
       return 0;
     }
